08a885f1cc3495d85372ade1151dbb18df1d6152
urbain Balogou <urbainbalogou19@gmail.com>
2024-09-30 22:44:13 +0000

    restructuration et stylisation

--- location/main/urls.py
+++ location/main/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name="main"),
+    path('nos_Services/', views.Services, name="nos_Services"),
 ]
--- location/main/views.py
+++ location/main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+def Services(request):
+    return render(request, 'main/Services.html')
